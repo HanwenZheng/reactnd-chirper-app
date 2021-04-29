b62613eb47916d4d92aff70bc59ea094ffa4163c
Alex Zheng <alex.zhenghw@gmail.com>
2021-04-29 22:19:56 +1000

    Production: release v1.0

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 
 import reducer from "./reducers";
 import middleware from "./middleware";
